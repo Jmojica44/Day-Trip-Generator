e3b0af7ddf64f310525986971d8506b1ec9d840c
Jonathan Mojica <Jonathan.Mojica44@gmail.com>
2023-02-10 15:18:10 -0600

    Added main and notes files

---
+++ notes.py
@@ -0,0 +1,36 @@
+# (5 points): As a developer, I want to make at least three commits with descriptive messages 
+# (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
+    # string values ex. "New York City", "San Francisco"
+# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, 
+# and/or form of entertainment if I don’t like one or more of those things.
+# (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+# (10  points): As a user, I want to display my completed trip in the console
+# (5 points): Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing! 
+
+# def run_day_trip_generator():
+# def print_full_trip(list of options):
+# def generate_random_item(list of items):
+# def determine_satisfaction(current_trip, trip_options):
+# def re_select_option(current_trip, options):
+# output:
+    # Destination: Richmond
+    # Restaurant:
+    # Entertainment:
+    # Transportation:
+    # Are you satisfied with you trip? Y or N
+    # Y 
+        # Here is your final trip!
+            # Destination: Richmond
+            # Restaurant:
+            # Entertainment:
+            # Transportation:
+            # Are you satisfied with you trip? Y or N
+    # N
+        # Which option would you like to change?
+        # Destination
+        # Updates selected list
+    # or regenerate trip
